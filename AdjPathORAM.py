b4e95f3820501e735569beace3b033b2d365910e
rudedude2015501 <rudedude2015501@gmail.com>
2025-03-06 20:37:44 -0800

    made stuff for SEAL

---
+++ AdjPathORAM.py
@@ -0,0 +1,8 @@
+#Rudy Chavez 3/6/25
+#meant to create and implement the Path ORAM being adjustable
+#also stores function used for creating leackage within a data block
+
+class AdjORAM():
+
+    def __init__(self,BitsToLeak:int = 0, ValuesToStore:int =10,  ) -> None:
+        
